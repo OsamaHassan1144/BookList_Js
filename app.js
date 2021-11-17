da24185b33630d5c2bcf76a0735272141c466c54
usamahassan00015 <usama.hassan@visiomate.com>
2021-11-17 13:16:42 +0500

    Add Book To List

---
+++ app.js
@@ -0,0 +1,58 @@
+//Book Constructor
+function Book(title,author,isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+//UI Constructor
+function UI(){}
+
+//Add book to list
+UI.prototype.addBookToList = function(book){
+    // console.log(book);
+    const list = document.getElementById('book-list');
+    //Create tr element
+    const row = document.createElement('tr');
+    // const data = document.createElement('td');
+
+    // console.log(row);
+    //Instantiate values of book element
+    // a. Title
+    // b. author
+    // c. isbn
+    row.innerHTML = 
+    `   <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>`;
+
+}
+//Clear Fields
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '',
+    document.getElementById('author').value = '',
+    document.getElementById('isbn').value = ''
+}
+
+//Event Listeners
+document.getElementById('book-form').addEventListener('submit',function(e){
+    e.preventDefault();
+    //Get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+    // console.log(title,author,isbn);
+
+    //Instantiate book
+    const book = new Book(title, author, isbn);
+
+    //Instantiate UI
+    const ui = new UI();
+
+    //Add book to list
+    ui.addBookToList(book);
+    //Clear fields
+    ui.clearFields();
+
+})
